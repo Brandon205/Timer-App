426f1c72995c2cdd2141834c7cc2a4bc276cc519
Brandon205 <brandonblack02@gmail.com>
2019-12-11 11:00:44 -0800

    try7

--- client/src/App.js
+++ client/src/App.js
@@ -13,7 +13,7 @@ export default function App() {
   const [token, setToken] = useState('');
   const [user, setUser] = useState(null);
 
-  const client = new ApolloClient({ uri: 'http://localhost:3001/graphql' });
+  const client = new ApolloClient({ uri: 'https://sheltered-reef-38980.herokuapp.com//graphql' });
 
   let checkForLocalToken = () => {
     // Look in LS for localtoken
